4268f39a8ad28d88fa1d2a33cb17d64b36f15cb1
benjaminbtrance <benjaminb.tran@gmail.com>
2021-06-07 19:33:13 -0500

    add the display fucntion into a document.ready function. add a onclick dunction to the save button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-var currentDate = $('#currentDay');
+$(document).ready(function () {
+	var currentDate = $('#currentDay');
 
-// handle displaying the date and time
-function displayTime() {
-	var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-	currentDate.text(rightNow);
-}
+	// handle displaying the date and time
+	function displayTime() {
+		var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+		currentDate.text(rightNow);
+	}
 
-setInterval(displayTime, 1000);
+	setInterval(displayTime, 1000);
+
+	$('.saveBtn').on('click', function () {
+		var input = $(this).siblings('.input-text').val();
+		var time = $(this).parent().attr('id');
+		console.log(this);
+		console.log(input);
+		console.log(time);
+
+		localStorage.setItem(time, input);
+	});
+});
